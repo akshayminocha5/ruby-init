3f4b9c9f2dc4523fe0a2aecb4fd2dd36ac63147a
Akshay <akshay@Akshay-MacBook-Pro.local>
2017-01-24 14:27:38 +0530

    removed redundant head

--- bash/log_file.sh
+++ bash/log_file.sh
@@ -1,1 +1,1 @@
-cut -d":" -f4 tmp_more_than_50 | awk 'NR%3==2'| sed 's/Completed [0-9]\{3\}//g' | sed 's/[^0-9]*//g' | sort -nr | head -1 | echo $(head -1)"ms"
+cut -d":" -f4 tmp_more_than_50 | awk 'NR%3==2'| sed 's/Completed [0-9]\{3\}//g' | sed 's/[^0-9]*//g' | sort -nr | echo $(head -1)"ms"
